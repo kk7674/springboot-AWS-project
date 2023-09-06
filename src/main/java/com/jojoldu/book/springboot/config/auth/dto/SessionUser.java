fb5bf9207780ad3a985c17510281f03eee7470c6
TAEUKKIM <kkw7674@naver.com>
2023-09-06 21:24:01 +0900

    Chapter 5.3 - 구글 로그인 구현 ing

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.jojoldu.book.springboot.config.auth.dto;
+
+import com.jojoldu.book.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = getName();
+        this.email = getEmail();
+        this.picture = getPicture();
+    }
+}
